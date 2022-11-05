1bf40dd81f31020d78068bb7327144fafe91a1e6
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-11-05 12:38:14 +1000

    Feature: edit settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', default='localhost'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
-} 
+}
 
 
 # Password validation
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
